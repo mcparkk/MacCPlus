65243f7c57898f1ad375e966b082e2fd252462a1
mcparkk <pmc0228@naver.com>
2020-11-23 17:22:41 +0900

    예외 수정

--- countOfFriends.cpp
+++ countOfFriends.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+//#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int friendList[] = {8, 4, 5, 2, 1, 7, 4, 5, 8};
+int friendList[] = {0, 1, 5, 2, 1, 7, 4, 5, 8};
 //int friendList[] = {1, 2, 1, 1, 1, 1, 1, 1, 1};
 
 int cnt = 0;
@@ -10,8 +11,9 @@ int cnt = 0;
 int GetCountOfFriends(int friendsList[], int arrCount)
 {
     for (int i = 0; i < arrCount; i++)
-    {
-        if (friendsList[i] < i && friendsList[i - friendsList[i]] != friendsList[i])
+    {   
+        if(friendList[i] == 0)continue;
+        if (friendsList[i] <= i && friendsList[i - friendsList[i]] != friendsList[i])
             cnt++;
         if (friendsList[i] < arrCount-i)
             cnt++;
